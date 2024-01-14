fa3fa399f0428c0a89e85045148873bb7375eecf
JHRL777 <134742412+JHRL777@users.noreply.github.com>
2024-01-14 09:33:31 -0500

    comandos basicos estructura

---
+++ Statements/order_by.sql
@@ -0,0 +1,7 @@
+SELECT * FROM users ORDER BY age DESC;
+
+SELECT * FROM users ORDER BY age ASC;
+
+SELECT * FROM users WHERE email="jhre@programdorfull.com" ORDER BY age DESC;
+SELECT * FROM users  WHERE email="jhre@programdorfull.com" ORDER BY age ASC;
+SELECT name FROM users  WHERE email="jhre@programdorfull.com" ORDER BY age ASC;
---
+++ Statements/where.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE age = 15;
+
+SELECT * FROM name WHERE age = 15;
+
+SELECT DISTINCT name FROM users WHERE age = 15;
